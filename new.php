f88a4604933906a46de0da866870fb152f1c1d6c
SDen4 <asw325@yandex.ru>
2020-07-06 23:06:55 +0500

    add a new record to DB

---
+++ new.php
@@ -0,0 +1,18 @@
+<?php
+    // connecting to BD
+    require_once 'connecting.php';
+?>
+
+<?php
+    $http_origin = $_SERVER['HTTP_ORIGIN'];
+    if ($http_origin == "http://localhost:8082") {header("Access-Control-Allow-Origin: $http_origin");}
+
+    if(isset($_POST["bouquetName"]) && isset($_POST["bouquetPrice"]) && isset($_POST["bouquetDescription"]) && isset($_POST["bouquetPic"])) {
+        $new = "INSERT INTO `bouquets_list` (`name`, `price`, `description`, `pic`) VALUES ('{$_POST["bouquetName"]}', '{$_POST["bouquetPrice"]}', '{$_POST["bouquetDescription"]}', '{$_POST["bouquetPic"]}')";
+        if(mysqli_query($mysqli, $new)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $new . "<br>" . mysqli_error($mysqli);
+        };
+    }
+?>
