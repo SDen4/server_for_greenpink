fbf91ebe87adfc6088e17b0fc1b3522da69a6158
SDen4 <asw325@yandex.ru>
2020-07-13 23:03:26 +0500

    bug in code of international characters in new bouquets

--- new.php
+++ new.php
@@ -7,6 +7,9 @@
     $http_origin = $_SERVER['HTTP_ORIGIN'];
     if ($http_origin == "http://localhost:8082") {header("Access-Control-Allow-Origin: $http_origin");}
 
+    // it solve the problem of displaying international characters
+    $mysqli->set_charset('utf8');
+
     if(isset($_POST["bouquetName"]) && isset($_POST["bouquetPrice"]) && isset($_POST["bouquetDescription"]) && isset($_POST["bouquetPic"])) {
         $new = "INSERT INTO `bouquets_list` (`name`, `price`, `description`, `pic`) VALUES ('{$_POST["bouquetName"]}', '{$_POST["bouquetPrice"]}', '{$_POST["bouquetDescription"]}', '{$_POST["bouquetPic"]}')";
         if(mysqli_query($mysqli, $new)) {
